b53b87edb6e4b90b89416eb2115646549a2388b1
Merty <183492891+MakarIKA-web@users.noreply.github.com>
2026-03-20 08:54:13 +0100

    css

--- login.php
+++ login.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="no" >
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="src/css/login.css">
+<link rel="stylesheet" href="src/css/logi.css">
 <title>Logg inn</title>
 </head>
 <body>
